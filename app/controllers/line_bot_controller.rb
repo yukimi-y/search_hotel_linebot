2417cd3e1175f1c4f02fd5ad7bf3c01732b3e49d
yukimi.y <yyy.yukimi@gmail.com>
2021-04-06 09:21:45 +0900

    Line::Bot::Clientクラスをインスタンス化

--- app/controllers/line_bot_controller.rb
+++ app/controllers/line_bot_controller.rb
@@ -1,7 +1,15 @@
 class LineBotController < ApplicationController
   protect_from_forgery except: [:callback]
-  
-  def callback
 
+  def callback
   end
+
+  private
+
+    def client
+      @client ||= Line::Bot::Client.new { |config|
+        config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+        config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+      }
+    end
 end
